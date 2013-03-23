5d2ae67cae324154e5b0776099cfbec9a3338b0f
Minakshi Korad <mckorad@ncsu.edu>
2013-03-22 23:09:33 -0400

    folder commit

--- gen/com/example/spinningwellness/R.java
+++ gen/com/example/spinningwellness/R.java
@@ -11,21 +11,32 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bg=0x7f020000;
+        public static final int icon=0x7f020001;
+        public static final int logo=0x7f020002;
+        public static final int repeat_bg=0x7f020003;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int btnLogin=0x7f060002;
+        public static final int btnRegister=0x7f060007;
+        public static final int footer=0x7f060001;
+        public static final int header=0x7f060000;
+        public static final int link_to_login=0x7f060008;
+        public static final int link_to_register=0x7f060003;
+        public static final int reg_email=0x7f060005;
+        public static final int reg_fullname=0x7f060004;
+        public static final int reg_password=0x7f060006;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-    }
-    public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int footer_repeat=0x7f030000;
+        public static final int header_gradient=0x7f030001;
+        public static final int login=0x7f030002;
+        public static final int main=0x7f030003;
+        public static final int register=0x7f030004;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int app_name=0x7f040001;
+        public static final int hello=0x7f040000;
     }
     public static final class style {
         /** 
