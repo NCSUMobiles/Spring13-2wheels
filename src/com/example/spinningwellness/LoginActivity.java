58df12f4c3006f66dbef43555f3fce9666e9c271
pshegde@ncsu.edu <pshegde@ncsu.edu>
2013-03-27 23:27:52 -0400

    Added login via wordpress

--- src/com/example/spinningwellness/LoginActivity.java
+++ src/com/example/spinningwellness/LoginActivity.java
@@ -1,5 +1,12 @@
 package com.example.spinningwellness;
 
+import java.util.List;
+import java.net.MalformedURLException;
+
+import redstone.xmlrpc.XmlRpcFault;
+
+import net.bican.wordpress.User;
+import net.bican.wordpress.Wordpress;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -20,6 +27,22 @@ public class LoginActivity extends Activity {
 			public void onClick(View v) {
 				// Switching to Register screen
 				Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
+				String password = "ncsuspr2013";
+				String username = "spinningwellness";
+			    String xmlRpcUrl = "http://spinningwellness.wordpress.com/xmlrpc.php";
+			    try {
+			    	System.setProperty("org.xml.sax.driver","org.xmlpull.v1.sax2.Driver");
+					Wordpress wp = new Wordpress(username, password, xmlRpcUrl);
+					User u = wp.getUserInfo();
+					System.out.println(u.getFirstname());
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (XmlRpcFault e) {
+					// TODO Auto-generated catch block
+					android.util.Log.v("WPJAVA", "Exception ["+e.getMessage()+"]", e);
+					e.printStackTrace();
+				}
 				startActivity(i);
 			}
 			
