5ec53ca604b5f1e301eab556a24c369d5b02495c
Minakshi Korad <mckorad@ncsu.edu>
2013-03-28 22:25:00 -0400

    spinning wellness

--- gen/com/example/spinningwellness/R.java
+++ gen/com/example/spinningwellness/R.java
@@ -10,6 +10,16 @@ package com.example.spinningwellness;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        /**  Default screen margins, per the Android Design guidelines. 
+
+         Customize dimensions originally defined in res/values/dimens.xml (such as
+         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
+    
+         */
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
+    }
     public static final class drawable {
         public static final int bg=0x7f020000;
         public static final int icon=0x7f020001;
@@ -17,31 +27,44 @@ public final class R {
         public static final int repeat_bg=0x7f020003;
     }
     public static final class id {
-        public static final int btnLogin=0x7f060005;
-        public static final int btnRegister=0x7f06000a;
-        public static final int button1=0x7f06000c;
-        public static final int footer=0x7f060001;
-        public static final int header=0x7f060000;
-        public static final int link_to_login=0x7f06000b;
-        public static final int login_layout=0x7f060002;
-        public static final int login_password=0x7f060004;
-        public static final int login_username=0x7f060003;
-        public static final int post_blog=0x7f060006;
-        public static final int reg_email=0x7f060008;
-        public static final int reg_fullname=0x7f060007;
-        public static final int reg_password=0x7f060009;
+        public static final int action_settings=0x7f080010;
+        public static final int bottomLayout=0x7f080007;
+        public static final int btnLogin=0x7f080005;
+        public static final int btnRegister=0x7f08000d;
+        public static final int button1=0x7f08000f;
+        public static final int footer=0x7f080001;
+        public static final int header=0x7f080000;
+        public static final int link_to_login=0x7f08000e;
+        public static final int listView1=0x7f080006;
+        public static final int login_layout=0x7f080002;
+        public static final int login_password=0x7f080004;
+        public static final int login_username=0x7f080003;
+        public static final int post_blog=0x7f080009;
+        public static final int reg_email=0x7f08000b;
+        public static final int reg_fullname=0x7f08000a;
+        public static final int reg_password=0x7f08000c;
+        public static final int textView1=0x7f080008;
     }
     public static final class layout {
-        public static final int footer_repeat=0x7f030000;
-        public static final int header_gradient=0x7f030001;
-        public static final int login=0x7f030002;
-        public static final int main=0x7f030003;
-        public static final int register=0x7f030004;
+        public static final int activity_join=0x7f030000;
+        public static final int footer_repeat=0x7f030001;
+        public static final int header_gradient=0x7f030002;
+        public static final int join_list=0x7f030003;
+        public static final int login=0x7f030004;
+        public static final int main=0x7f030005;
+        public static final int register=0x7f030006;
+    }
+    public static final class menu {
+        public static final int join=0x7f070000;
     }
     public static final class string {
-        public static final int _17=0x7f040002;
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int _17=0x7f050002;
+        public static final int action_settings=0x7f050004;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
+        public static final int hello_world=0x7f050005;
+        public static final int some_text=0x7f050006;
+        public static final int title_activity_join=0x7f050003;
     }
     public static final class style {
         /** 
@@ -65,10 +88,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
