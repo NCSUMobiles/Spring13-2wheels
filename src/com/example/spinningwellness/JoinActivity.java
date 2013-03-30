1826a5a8b4f15a42bb6f33c361dbafbf36071c85
pshegde <Saraswati5>
2013-03-30 16:03:37 -0400

    added call in join activity to get rides

--- src/com/example/spinningwellness/JoinActivity.java
+++ src/com/example/spinningwellness/JoinActivity.java
@@ -2,7 +2,10 @@ package com.example.spinningwellness;
 
 import java.util.ArrayList;
 import java.util.List;
- 
+
+import com.ncsu.edu.spinningwellness.entities.Ride;
+import com.ncsu.edu.spinningwellness.managers.RidesManager;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -13,65 +16,82 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
- 
+
 public class JoinActivity extends Activity {
-     
- @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-     
-        //Generate list View from ArrayList
-  displayListView();
-  
- }
-  
- private void displayListView() {
- 
-  //Array list of Rides
-  List<String> rideList = new ArrayList<String>();
-  rideList.add("RtoDurham");
-  rideList.add("RtoCary");
-  rideList.add("RtoGreensboro");
-  rideList.add("RtoLakeRaleigh");
-  rideList.add("RtoRTP");
-//  countryList.add("Belize");
-//  countryList.add("Bermuda");
-//  countryList.add("Barbados");
-//  countryList.add("Canada");
-//  countryList.add("Costa Rica");
-//  countryList.add("Cuba");
-//  countryList.add("Cayman Islands");
-//  countryList.add("Dominica");
-//  countryList.add("Dominican Republic");
-//  countryList.add("Guadeloupe");
-//  countryList.add("Grenada");
-//  countryList.add("Greenland");
-//  countryList.add("Guatemala");
-//  countryList.add("Honduras");
-//  countryList.add("Haiti");
-//  countryList.add("Jamaica");
-   
-  //create an ArrayAdaptar from the String Array
-  ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
-    R.layout.join_list, rideList);
-  ListView listView = (ListView) findViewById(R.id.listView1);
-  // Assign adapter to ListView
-  listView.setAdapter(dataAdapter);
-   
-  //enables filtering for the contents of the given ListView
-  listView.setTextFilterEnabled(true);
- 
-  listView.setOnItemClickListener(new OnItemClickListener() {
-   public void onItemClick(AdapterView<?> parent, View view,
-     int position, long id) {
-       // When clicked, show a toast with the TextView text
-    Toast toast = Toast.makeText(getApplicationContext(),
-      ((TextView) view).getText(), Toast.LENGTH_LONG);
-    toast.setGravity(Gravity.TOP, 25, 300); 
-    toast.show();
-   }
-  });
-   
- }
+
+	List<String> rideNameList = new ArrayList<String>();
+	List<Ride> rideList = new ArrayList<Ride>();
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		//Generate list View from ArrayList
+		displayListView();
+
+	}
+
+	private void displayListView() {
+
+
+		//Array list of Rides
+		rideList = RidesManager.viewUpcomingRides();
+		for(Ride r:rideList){
+			rideNameList.add(r.getName());
+		}
+
+		//  rideList.add("RtoDurham");
+		//  rideList.add("RtoCary");
+		//  rideList.add("RtoGreensboro");
+		//  rideList.add("RtoLakeRaleigh");
+		//  rideList.add("RtoRTP");
+		//  countryList.add("Belize");
+		//  countryList.add("Bermuda");
+		//  countryList.add("Barbados");
+		//  countryList.add("Canada");
+		//  countryList.add("Costa Rica");
+		//  countryList.add("Cuba");
+		//  countryList.add("Cayman Islands");
+		//  countryList.add("Dominica");
+		//  countryList.add("Dominican Republic");
+		//  countryList.add("Guadeloupe");
+		//  countryList.add("Grenada");
+		//  countryList.add("Greenland");
+		//  countryList.add("Guatemala");
+		//  countryList.add("Honduras");
+		//  countryList.add("Haiti");
+		//  countryList.add("Jamaica");
+
+		//create an ArrayAdaptar from the String Array
+		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+				R.layout.join_list, rideNameList);
+		ListView listView = (ListView) findViewById(R.id.listView1);
+		// Assign adapter to ListView
+		listView.setAdapter(dataAdapter);
+
+		//enables filtering for the contents of the given ListView
+		listView.setTextFilterEnabled(true);
+
+		listView.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// When clicked, show a toast with the TextView text
+				Toast toast = Toast.makeText(getApplicationContext(),
+						((TextView) view).getText(), Toast.LENGTH_LONG);
+				toast.setGravity(Gravity.TOP, 25, 300); 
+				toast.show();
+				Ride selectedRide;
+				for(Ride r:rideList){
+					if(r.getName().equalsIgnoreCase(((TextView) view).getText().toString())){
+						selectedRide = r;
+						break;
+					}
+				}
+				
+				
+			}
+		});
+
+	}
 }
