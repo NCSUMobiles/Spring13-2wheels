efe4b8df90bae59c3d0550c6ee9f13572015c03c
pshegde <Saraswati5>
2013-03-30 21:37:03 -0400

    publish to blog and async task functionality done

--- src/com/example/spinningwellness/PostBlogActivity.java
+++ src/com/example/spinningwellness/PostBlogActivity.java
@@ -1,15 +1,45 @@
 package com.example.spinningwellness;
 
+import java.net.MalformedURLException;
+import java.util.List;
+
+
+import net.bican.wordpress.Page;
+import net.bican.wordpress.User;
+import net.bican.wordpress.Wordpress;
+import redstone.xmlrpc.XmlRpcFault;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.view.Gravity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class PostBlogActivity extends Activity {
 
+	String username;
+	String password;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_post_blog);
+
+		TextView postBlog = (TextView) findViewById(R.id.btnPostBlog);
+		postBlog.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				username = getIntent().getStringExtra("username");
+				password = getIntent().getStringExtra("password");
+				new PostBlogTask().execute();
+			}
+		});
 	}
 
 	@Override
@@ -17,6 +47,47 @@ public class PostBlogActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.post_blog, menu);
 		return true;
+	}
+
+	private class PostBlogTask extends AsyncTask<Void,Void,User> {
+		Exception error;
+		protected User doInBackground(Void... params) {
+			//System.out.println("********bckgrnd");
+			String xmlRpcUrl = "http://spinningwellness.wordpress.com/xmlrpc.php";
+			// Switching to Register screen
+
+			System.setProperty("org.xml.sax.driver","org.xmlpull.v1.sax2.Driver");
+			Wordpress wp;
+			try {
+				wp = new Wordpress(username, password, xmlRpcUrl);
+				List<Page> plist = wp.getPages();
+				for(Page p:plist){
+					int i = p.getPage_id();
+				}
+				Page recentPost = new Page();
+				recentPost.setDescription(((TextView) findViewById(R.id.txtexperience)).getText().toString());
+				String result = wp.newPost(recentPost, true);
+
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				error = e;
+			} catch (XmlRpcFault e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				error = e;
+			}
+			return null;
+		}
+
+		protected void onPostExecute(User result) {
+			//System.out.println("********post");
+			if(error != null){
+				Toast.makeText(PostBlogActivity.this, "Issue in posting to blog", Toast.LENGTH_SHORT).show();
+			} else{
+				Toast.makeText(PostBlogActivity.this, "Post Successful", Toast.LENGTH_SHORT).show();
+			}
+		}
 	}
 
 }
--- src/com/example/spinningwellness/RegisterActivity.java
+++ src/com/example/spinningwellness/RegisterActivity.java
@@ -24,7 +24,7 @@ public class RegisterActivity extends Activity {
         String username = getIntent().getStringExtra("username");
         System.out.println("Welcome " + username + "!");
        
-        TextView postBlog = (TextView) findViewById(R.id.post_blog);
+       
         
         // Listening to Login Screen link
 //        loginScreen.setOnClickListener(new View.OnClickListener() {
@@ -35,34 +35,5 @@ public class RegisterActivity extends Activity {
 //			}
 //		});
         
-        postBlog.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				String username = getIntent().getStringExtra("username");
-				String password = getIntent().getStringExtra("password");
-				String xmlRpcUrl = "http://spinningwellness.wordpress.com/xmlrpc.php";
-				try {
-					Wordpress wp = new Wordpress(username, password, xmlRpcUrl);
-					List<Page> plist = wp.getPages();
-					for(Page p:plist){
-						int i = p.getPage_id();
-						
-					}
-					 Page recentPost = new Page();
-					 recentPost.setDescription("Test post from java");
-					 String result = wp.newPost(recentPost, true);
-					 
-				} catch (MalformedURLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (XmlRpcFault e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				System.out.println();
-			}
-		});
     }
 }
