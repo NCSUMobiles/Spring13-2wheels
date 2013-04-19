be8ae624210cdfa368ad9cf323980edcba6755be
Amarja <amarjavaidya21@@gmail.com>
2013-04-18 21:29:57 -0400

    Added functionality to show leader board page.. Shows only top 3 users.. Screen looks empty.. Can be extended to show top 5 or top 10 from backend side..

--- src/com/example/spinningwellness/LeaderBoardActivity.java
+++ src/com/example/spinningwellness/LeaderBoardActivity.java
@@ -1,13 +1,27 @@
 package com.example.spinningwellness;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ncsu.edu.customadapters.UserCustomAdapter;
+import com.ncsu.edu.spinningwellness.entities.User;
+import com.ncsu.edu.spinningwellness.managers.UsersManager;
 import com.ncsu.edu.tabpanel.MenuConstants;
 import com.ncsu.edu.tabpanel.MyTabHostProvider;
 import com.ncsu.edu.tabpanel.TabView;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class LeaderBoardActivity extends BaseActivity {
+
+	List<User> topRiders = new ArrayList<User>();
+	List<String> users = new ArrayList<String>();
+	private LinearLayout progressBar;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -18,11 +32,52 @@ public class LeaderBoardActivity extends BaseActivity {
 		TabView tabView = tabProvider.getTabHost(MenuConstants.LEADER_BOARD);
 		tabView.setCurrentView(R.layout.leader_board_activity);
 		setContentView(tabView.render());			
+		
+		progressBar = (LinearLayout) findViewById(R.id.leaderBoardSpinner);
+		progressBar.setVisibility(View.VISIBLE);
+
+		new GetLeaderBoardTask().execute();
 	}
 
 	@Override
 	public void setTitle() {
 		final TextView myTitleText = (TextView)findViewById(R.id.myTitle);
 		myTitleText.setText(SPINNING_WEELNESS + " " + "Leader Board");		
+	}
+
+	private void displayListView() {
+
+		progressBar.setVisibility(View.INVISIBLE);
+
+		//create an ArrayAdaptar from the String Array
+		UserCustomAdapter dataAdapter = new UserCustomAdapter(this, R.id.leaderBoardTextVal, users);
+		final ListView listView = (ListView) findViewById(R.id.leaderBoardListView);
+
+		// Assign adapter to ListView
+		listView.setAdapter(dataAdapter);
+
+		//enables filtering for the contents of the given ListView
+		listView.setTextFilterEnabled(true);
+	}
+
+
+	public class GetLeaderBoardTask extends AsyncTask<Void,Void,List<User>> {
+		Exception error;
+
+		protected List<User> doInBackground(Void... params) {
+			return UsersManager.viewTopPerformers();
+		}
+
+		protected void onPostExecute(List<User> result) {
+			if(error != null){
+
+			} else {
+				topRiders = result;
+				for(User r: topRiders){
+					users.add(r.getName());
+				}				
+				displayListView();
+			}
+		}
 	}
 }
