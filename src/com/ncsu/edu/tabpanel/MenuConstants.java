10cf39bd1688c67d56b02488e6a3056b354b9034
pshegde@ncsu.edu <pshegde@ncsu.edu>
2013-04-18 22:45:23 -0400

    added background in common_layout..

--- src/com/ncsu/edu/tabpanel/MenuConstants.java
+++ src/com/ncsu/edu/tabpanel/MenuConstants.java
@@ -2,7 +2,7 @@ package com.ncsu.edu.tabpanel;
 
 public interface MenuConstants {
 	
-	public final String JOIN_RIDES = "Join Rides";
+	public final String JOIN_RIDES = "Home";
 	public final String PAST_RIDES = "Past Rides";
 	public final String CREATE_RIDE = "Create Ride";
 	public final String LEADER_BOARD = "Leader Board";
