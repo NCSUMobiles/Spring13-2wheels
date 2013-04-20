c1ff599a0a332cf92b32727353d336c0e08222b6
pshegde <Saraswati5>
2013-04-20 03:00:26 -0400

    changes to make ride comparable

--- src/com/ncsu/edu/customadapters/CustomEntry.java
+++ src/com/ncsu/edu/customadapters/CustomEntry.java
@@ -1,10 +1,30 @@
 package com.ncsu.edu.customadapters;
 
-public class CustomEntry {
+public class CustomEntry implements Comparable<CustomEntry> {
+	private String rideid;
+	private long startDate;
+	public long getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(long startDate) {
+		this.startDate = startDate;
+	}
+
+	public String getRideid() {
+		return rideid;
+	}
+
+	public void setRideid(String rideid) {
+		this.rideid = rideid;
+	}
+
 	private String textVal;
 	private boolean isJoined;
 
-	public CustomEntry(String string, boolean string2) {
+	public CustomEntry(String rideid,long startDate,String string, boolean string2) {
+		this.startDate = startDate;
+		this.rideid = rideid;
 		this.textVal = string;
 		this.isJoined = string2;
 	}
@@ -23,6 +43,17 @@ public class CustomEntry {
 
 	public void setJoined(boolean isJoined) {
 		this.isJoined = isJoined;
+	}
+
+	@Override
+	public int compareTo(CustomEntry another) {
+		if(this.startDate < another.startDate){
+			return 1;
+		} else if(this.startDate > another.startDate) {
+			return -1;	
+		} else{
+			return 0;
+		}
 	}
 	
 }
--- src/com/ncsu/edu/spinningwellness/entities/Ride.java
+++ src/com/ncsu/edu/spinningwellness/entities/Ride.java
@@ -7,7 +7,32 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 @XmlRootElement
-public class Ride extends BaseEntity implements Parcelable{
+public class Ride extends BaseEntity implements Parcelable,Comparable<Ride>{
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Ride other = (Ride) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 
 	String id;
 	String name;
@@ -126,4 +151,15 @@ public class Ride extends BaseEntity implements Parcelable{
 	    }
 
 	};
+
+	@Override
+	public int compareTo(Ride another) {
+		if(this.startTime < another.startTime){
+			return 1;
+		} else if(this.startTime > another.startTime) {
+			return -1;	
+		} else{
+			return 0;
+		}
+	}
 }
